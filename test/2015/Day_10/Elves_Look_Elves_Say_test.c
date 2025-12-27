488ddff5d9fd686987e2e0860c99be89e1adb685
MHutten <milanhutten@live.nl>
2025-12-27 07:32:59 +0100

    Added unit tests for Day 10 2015

---
+++ test/2015/Day_10/Elves_Look_Elves_Say_test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "../../../include/2015/Day_10/Elves_Look_Elves_Say.h"
+#include "../../../include/testing/asserts.h"
+#include "../../../include/testing/suite.h"
+
+struct TestOutput test__look_and_say__good_weather__1() {
+    struct TestOutput output = {
+        __func__,
+        assert_equal(look_and_say("1"), "11")
+    };
+    return output;
+}
+
+struct TestOutput test__look_and_say__good_weather__11() {
+    struct TestOutput output = {
+        __func__,
+        assert_equal(look_and_say("11"), "21")
+    };
+    return output;
+}
+
+struct TestOutput test__look_and_say__good_weather__21() {
+    struct TestOutput output = {
+        __func__,
+        assert_equal(look_and_say("21"), "1211")
+    };
+    return output;
+}
+
+struct TestOutput test__look_and_say__good_weather__1211() {
+    struct TestOutput output = {
+        __func__,
+        assert_equal(look_and_say("1211"), "111221")
+    };
+    return output;
+}
+
+struct TestOutput test__look_and_say__good_weather__111221() {
+    struct TestOutput output = {
+        __func__,
+        assert_equal(look_and_say("111221"), "312211")
+    };
+    return output;
+}
+
+int main() {
+    test_function test_functions[] = {
+        &test__look_and_say__good_weather__1,
+        &test__look_and_say__good_weather__11,
+        &test__look_and_say__good_weather__21,
+        &test__look_and_say__good_weather__1211,
+        &test__look_and_say__good_weather__111221
+    };
+    run(test_functions, 5);
+}
